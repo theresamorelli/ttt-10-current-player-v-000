d11ecb6ef87adb2aa629da37914f17f6a5bed143
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-10 00:48:49 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  moves = 0
+  board.each do |square|
+    if square == "O" || square ==
+      "X"
+      moves += 1
+    end
+  end
+  moves
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
